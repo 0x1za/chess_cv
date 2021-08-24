8fdb4a893e6b163b23346b4f8c326c304541f0b8
Mwiza Simbeye <mwiza@agripredict.com>
2021-08-24 11:50:37 +0200

    Benchmarking

--- camera/calibrate.cc
+++ camera/calibrate.cc
@@ -1,6 +1,26 @@
-//
-// Created by Mwiza Simbeye on 01/04/2020.
-//
+/*
+ * This file is part of chess_cv.camera.calibrate
+ *
+ * Created by Mwiza Simbeye on 01/04/2020.
+ *
+ * This program calibrates cameras.
+ * See the COPYRIGHT file at the top-level directory of this distribution
+ * for details of code ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "opencv2/opencv.hpp"
@@ -18,6 +38,10 @@ ABSL_FLAG(int, n_boards, 10, "Number of boards to calibrate");
 ABSL_FLAG(float, image_sf, 0.5f, "Image sf");
 ABSL_FLAG(int, board_w, 7, "Board width");
 ABSL_FLAG(int, board_h, 9, "Board height");
+ABSL_FLAG(bool, use_fisheye, false, "Use fish eye camera");
+ABSL_FLAG(bool, use_rtsp, false, "Capture video from RTSP");
+ABSL_FLAG(string, rtsp_url, "rtsp://192.168.1.26:8554/unicast",
+          "URL for RTSP video.");
 
 int main(int argc, char **argv) {
   // Initialise flag parsing.
@@ -28,17 +52,58 @@ int main(int argc, char **argv) {
   int board_h = absl::GetFlag(FLAGS_board_h);
   int n_boards = absl::GetFlag(FLAGS_n_boards);
   float image_sf = absl::GetFlag(FLAGS_image_sf);
+  bool use_rtsp = absl::GetFlag(FLAGS_use_rtsp);
+  bool use_fisheye = absl::GetFlag(FLAGS_use_fisheye);
+  string rtsp_url = absl::GetFlag(FLAGS_rtsp_url);
 
   // Calculate the board size
   int board_n = board_w * board_h;
 
+  VideoCapture capture;
+
   Size board_sz = Size(board_w, board_h);
 
-  // Start video capture
-  VideoCapture capture(0);
-  if (!capture.isOpened()) {
-    cout << "\nCouldn't open the camera\n";
-    return -1;
+  if (use_rtsp) {
+    VideoCapture capture(rtsp_url);
+    if (!capture.isOpened()) {
+      cout << "\nCouldn't open the camera\n";
+      return -1;
+    } else if (capture.isOpened()) {
+      cout << "RTSP Stream Live from " << rtsp_url << endl;
+    }
+
+    double fps = capture.get(CAP_PROP_FPS);
+    string d_fps = "Frames per seconds: ";
+    d_fps += to_string(int(fps) / int(1000));
+
+    // Create window and title
+    String window_name = "Chess Stream";
+    namedWindow(window_name, WINDOW_NORMAL);
+
+    while (true) {
+      Mat frame;
+      bool success = capture.read(frame);
+
+      if (!success) {
+        cout << "\nStream at " << rtsp_url << " ended" << endl;
+        break;
+      }
+      putText(frame, d_fps, Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.6,
+              Scalar(0, 200, 0), 2);
+      imshow(window_name, frame);
+
+      if (waitKey(10) == 27) {
+        cout << "\nESC key pressed, exiting stream..." << endl;
+        break;
+      }
+    }
+  } else {
+    // Start video capturei with default camera.
+    VideoCapture capture(0);
+    if (!capture.isOpened()) {
+      cout << "\nCouldn't open the camera\n";
+      return -1;
+    }
   }
 
   // Allocate storage, it is important to have the image and object points for
@@ -82,14 +147,14 @@ int main(int argc, char **argv) {
         opts[j] = Point3f((float)(j / board_w), (float)(j % board_w), 0.f);
       }
       cout << "Collected our " << (int)image_points.size() << " of " << n_boards
-           << " needed chessboard images"
-           << endl;
+           << " needed chessboard image(s)" << endl;
     }
     imshow("Calibration", image);
     if ((waitKey(30) & 255) == 27)
       return -1;
   }
 
+  // Destroy calibration preview window.
   destroyWindow("Calibration");
   cout << "\n*** CALIBRATING CAMERA...\n" << endl;
 
@@ -111,7 +176,7 @@ int main(int argc, char **argv) {
 
   // Load matrix back in!!
   fs.open("intrinsics.xml", FileStorage::READ);
-  cout << "\nimage_width: " << (int)fs["image_width"];
+  cout << "\nimage_width: " << static_cast<int>(fs["image_width"]);
   cout << "\nimage_height: " << (int)fs["image_height"];
 
   Mat intrinsic_matrix_loaded, distortion_coeffs_loaded;
